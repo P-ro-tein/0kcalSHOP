d8d51d4c31f3f044e3f3f939abefeafd7ad0f786
dbtkd1801@gmail.com <Yoosang>
2021-05-11 02:27:11 +0900

    [ENH] 일부 버그 해결(유저스키마 수정)/장바구니 기능 추가

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,17 +2,12 @@ const { User } = require("../models/User");
 
 let auth = (req, res, next) => {
   let token = req.cookies.x_auth;
-
-  User.findByToken(token)
-    .then((user) => {
+  User.findByToken(token, (err, user) => {
       if (!user) return next();
       req.token = token;
       req.user = user;
       next();
     })
-    .catch((err) => {
-      throw err;
-    });
 };
 
 module.exports = { auth };
