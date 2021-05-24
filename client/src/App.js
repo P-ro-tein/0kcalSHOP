0a10103a0879d1bf4d38d9aae0a2b42806485664
akrudal <lottie1255@gmail.com>
2021-05-25 01:45:52 +0900

    [BUG]충돌 해결

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Route, BrowserRouter } from 'react-router-dom';
-import { SEProvider } from './Context/Context'
 import Client from './Client';
 import Admin from './Admin/Admin';
 
 function App () {
     return (
         <BrowserRouter>
-            <SEProvider>
                 <Route path="/client" component={Client}/>
                 <Route path="/admin" component={Admin}/>
-            </SEProvider>
         </BrowserRouter>
     )
 }
