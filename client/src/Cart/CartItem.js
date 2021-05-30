c9f5e8907805266e7046117f692d3018c85a41f8
Yoosang <dbtkd1801@gmail.com>
2021-05-30 22:15:50 +0900

    [ENH] 장바구니 추가 및 장바구니 페이지 서버 연동

--- client/src/Cart/CartItem.js
+++ client/src/Cart/CartItem.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React,{useEffect,useState} from "react";
 import styled from 'styled-components';
 
@@ -44,16 +45,38 @@ const ItemPrice=styled.div`
     border-bottom:#D8D8D8 0.5px solid;
 `;
 function CartItem({Item}){
-
+    const [item,setItem] = useState({});
     const [quantity,setQuantity]=useState(Item.quantity);
     
-
+    useEffect(() => {
+        axios.get(`/api/product/products_by_id?id=${Item.id}&type=single`)
+        .then(res=>{
+                setItem(res.data[0])
+        })
+        .catch(err => alert(err))
+    },[Item])
     const onIncrease=()=>{
-        setQuantity(number=>number+1);
+        axios.post('/api/users/addToCart',{
+            productId: Item.id,
+            quantity: 1
+        }).then(res => {
+            if(res.data.success){
+                setQuantity(number=>number+1);
+            }
+        })
     };
 
     const onDecrease=()=>{
-        setQuantity(number=>number-1);
+        if(quantity>1){
+            axios.post('/api/users/addToCart',{
+                productId: Item.id,
+                quantity: -1
+            }).then(res => {
+                if(res.data.success){
+                    setQuantity(number=>number-1);
+                }
+            })
+        }
     };
     
     return(
@@ -61,14 +84,17 @@ function CartItem({Item}){
             <div style={{padding:'100px 30px 0px 50px',borderBottom:'#D8D8D8 0.5px solid'}}>
             <input type="checkbox"></input>
             </div>
-            <ItemImg src={Item.imgUrl}></ItemImg>
-            <ItemName>{Item.name}</ItemName>
+            {
+                item.images&&item.images.length>0 &&
+                <ItemImg src={`http://ec2-52-79-226-115.ap-northeast-2.compute.amazonaws.com:9000/uploads/${item.images[0]}`}></ItemImg>
+            }
+            <ItemName>{item.title}</ItemName>
             <Number>
             <ItemNumber placeholder={quantity}></ItemNumber>
                 <button onClick={onIncrease} className="num" style={{height:'30px'}}>+</button>
                 <button onClick={onDecrease} className="num" style={{height:'30px'}}>-</button> 
             </Number>
-            <ItemPrice>{Item.price}</ItemPrice>
+            <ItemPrice>{item.price}</ItemPrice>
         </ItemContainer>
     );
 }
