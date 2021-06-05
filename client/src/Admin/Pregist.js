c5f1ab01ad14fa16faba0dd21ba66dfd9ab0db65
Yoosang <dbtkd1801@gmail.com>
2021-06-06 03:45:02 +0900

    [ENH]상품등록 완성 및 공지등록 일부 수정

--- client/src/Admin/Pregist.js
+++ client/src/Admin/Pregist.js
@@ -1,16 +1,19 @@
 import './Basic.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function Pregist(props) {
+const Pregist = (props) => {
     const [title, setTitle] = useState("");
     const [category, setCategory] = useState("");
-    const [price, setPrice] = useState("");
-    const [shipCharge, setShipCharge] = useState("");
-    const [remainStock, setRemainStock] = useState("");
+    const [price, setPrice] = useState(0);
+    const [shipCharge, setShipCharge] = useState(0);
+    const [remainStock, setRemainStock] = useState(0);
     const [description, setDescription] = useState("");
-    const [images, setImages] = useState("");
+    const [Images, setImages] = useState({});
 
+    useEffect(()=>{
+        console.log(Images);
+    },[Images])
     const titleChangeHandler = (e) => {
         setTitle(e.currentTarget.value);
     }
@@ -27,46 +30,48 @@ export default function Pregist(props) {
         setRemainStock(e.currentTarget.value);
     }
     const imagesChangeHandler = (e) => {
-        setImages(e.currentTarget.value);
-    }
-    const descriptionChangeHandler = (e) => {
-        setDescription(e.currentTarget.value)
+        // let productImages=[];
+        // for(let i=0;i<e.target.files.length;i+=1){
+        //     productImages.push(e.target.files[i]);
+        // }
+        // setImages(productImages);
+        setImages(e.target.files);
     }
     
-
+    const descriptionChangeHandler = (e) => {
+        setDescription(e.currentTarget.value);
+    }
+    
     const submitHandler = (event) => {
-        /*const data = {
-            title: title,
-            category: category,
-            price: price,
-            shipCharge: shipCharge,
-            remainStock: remainStock,
-            description: description,
-            images: images,
-        }*/
-        const formData = new FormData(); 
-
-        formData.append('title', title);
-        formData.append('category', category);
-        formData.append('price', price);
-        formData.append('shipCharge', shipCharge);
-        formData.append('remainStock', remainStock);
-        formData.append('description', description);
-        formData.append('images', event.target.images.files[0]);     
-        
-        axios.post(
-            '/api/product/register',
-            {
-                formData
+        event.preventDefault();
+        if(title&&category&&price&&shipCharge&&remainStock&&description){
+            let formData = new FormData(); 
+            const config = {
+                header: { 'content-type': 'multipart/form-data' }
             }
-        ).then(response => {
-            if(response.data.success){
-                alert('등록완료');
-                props.history.push('/admin/psearch');
-            } else { 
-                alert('');
-            }
-        });
+            formData.append('title', title);
+            formData.append('category', category);
+            formData.append('price', price);
+            formData.append('shipCharge', shipCharge);
+            formData.append('remainStock', remainStock);
+            formData.append('description', description);
+            formData.append('images',Images[0]);
+            formData.append('images',Images[1]);
+            formData.append('images',Images[2]);
+            
+            axios.post('/api/product/register', formData, config)
+            .then(res=>{
+                if(res.data.success){
+                    alert('등록 완료');
+                    props.history.push('/admin/psearch');
+                } else { 
+                    alert('등록 실패');
+                    props.history.push('/admin/psearch');
+                }
+            })
+        } else {
+            alert("정보를 입력해주세요.");  
+        }
     }
 
     return(
@@ -74,7 +79,7 @@ export default function Pregist(props) {
             <span><b>상품 관리 &gt; 상품 등록</b></span>
             <hr/>
 
-            <form action="/api/product/register" method="POST" encType="multipart/form-data">
+            <form>
             <table>
                 <tbody>
                 <tr className="Ttitle">
@@ -87,7 +92,7 @@ export default function Pregist(props) {
                     <td>카테고리명</td>
                     <td>
                         <select name="category" onChange={categoryChangeHandler}>
-                            <option selected disabeld hidden>카테고리 선택</option>
+                            <option defaultValue disabled hidden>카테고리 선택</option>
                             <option value="식단세트">식단세트</option>
                             <option value="식사대용">식사대용</option>
                             <option value="건강간식">건강간식</option>
@@ -108,19 +113,21 @@ export default function Pregist(props) {
                 </tr>
                 <tr>
                     <td>상품 소개</td>
-                    <td><input type="text" name="description" name="description" onChange={descriptionChangeHandler} /> </td>
+                    <td><input type="text" name="description" onChange={descriptionChangeHandler} /> </td>
                 </tr>
                 <tr>
-                    <td>대표이미지</td><td><input type="file" name="images" accept="img/*" onChange={imagesChangeHandler}/></td>
+                    <td>대표이미지</td><td><input type="file" name="images" accept="img/*" onChange={imagesChangeHandler} multiple/></td>
                 </tr>
                 <tr>
-                    <td>상품 상세설명</td><td><input type="file" name="images" accept="img/*" multiple onChange={imagesChangeHandler}/></td>
+                    <td>유의사항</td><td>이미지 두장은 썸네일 한장은 설명</td>
                 </tr>
                 </tbody>
             </table><br/>
             
-            <button type="submit" onClick={submitHandler}>등록</button>
+            <button onClick={submitHandler}>등록</button>
             </form>
         </div>
     )
 }
+
+export default Pregist
